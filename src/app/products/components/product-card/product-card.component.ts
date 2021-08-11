403ddb56fd6d9fcc51879948a66b302132d11507
Sepandar.d <sepandar.derakhshandeh@gmail.com>
2021-08-12 00:07:36 +0430

    [Refactor] change card's style add queryParams for pass data in url

--- src/app/products/components/product-card/product-card.component.ts
+++ src/app/products/components/product-card/product-card.component.ts
@@ -3,28 +3,27 @@ import { Router } from '@angular/router';
 import { environment } from 'src/configs/environment';
 import { ProductsService } from '../../shared/products.service';
 
-
-
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.css'],
+  styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent implements OnInit {
-  product: any[]    
-    IMAGE_URL : string = environment.IMAGE_URL
+  product: any[];
+  productId;
+  IMAGE_URL: string = environment.IMAGE_URL;
+  mouseEntered : boolean = false;
+  
 
   constructor(private productSrv: ProductsService, private router: Router) {}
 
   ngOnInit(): void {
-    console.log(this.productSrv.getProducts());
-    this.getProduct() 
+    this.getProduct();
   }
   onClinkToDetaliPage(item) {
-    this.router.navigate([
-      '/online-shop/products/product-detail',
-      { Id: item.prodcutId },
-    ]);
+    this.router.navigate(['/online-shop/products/product-detail'], {
+      queryParams: { Id: item.productid },
+    });
   }
   getProduct() {
     this.product = this.productSrv.getProducts();
